8e471bca00aa71c56a13477141da51968d223541
Marlon Andrés Navarrete S <marlon.navarrete@mercadolibre.com.co>
2023-06-13 11:03:18 -0500

    FT - Workshop - Initialize

---
+++ cypress/e2e/specs/workshop/level.1.cy.js
@@ -0,0 +1,11 @@
+describe('EMON', () => {
+
+  beforeEach(() => {
+    cy.viewport(1920,1080);
+    cy.visit('https://mercadolibre.com.ar');
+  });
+
+  it('...', () => {
+    // Level One
+  });   
+});
