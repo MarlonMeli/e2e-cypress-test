5f2fa45868db04027e94a89be49d5a194198c5ac
crisparraq <cristian.parraquintero@mercadolibre.com.co>
2023-06-14 13:07:44 -0500

    test level 2-3

--- cypress/e2e/specs/workshop/level.3.cy.js
+++ cypress/e2e/specs/workshop/level.3.cy.js
@@ -1,4 +1,4 @@
-describe('EMON', () => {
+describe('EMON LEVEL 3', () => {
 
   beforeEach(() => {
       cy.viewport(1920,1080);
@@ -7,5 +7,21 @@ describe('EMON', () => {
 
   it('...', () => {
       // Level Three
+      cy.Login();
+      cy.waitForReact();
+      cy.visit('https://mercadolibre.com.ar/publicaciones/editor-masivo');
+      cy.waitForReact();
+      cy.contains('Filtros');
+      cy.get('[type="button"]').contains('Filtros').click();
+      cy.waitForReact();
+      cy.get('[id="ACTIVE"]').click();
+      cy.get('[id="INACTIVE"]').click();
+      cy.get('.andes-button').contains('Aplicar').click();
+      cy.waitForReact();
+      cy.get('.sc-bulk-items-quantity__text').then($totalItems => {
+        const items = $totalItems.text();
+        cy.log(items);
+        expect(items).eq('562 publicaciones')
+      })
   }); 
 });
