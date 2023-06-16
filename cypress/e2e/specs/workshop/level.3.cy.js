a9ec48cb4e541e712eb7b2b226154da64371a0dd
Marlon Andrés Navarrete S <marlon.navarrete@mercadolibre.com.co>
2023-06-16 10:37:36 -0500

    Solucion Pipe

--- cypress/e2e/specs/workshop/level.3.cy.js
+++ cypress/e2e/specs/workshop/level.3.cy.js
@@ -1,55 +1,32 @@
-
-describe('EMON', () => {
-
-  beforeEach(() => {
-      cy.viewport(1920,1080);
-      cy.visit('https://mercadolibre.com.ar');
-  });
-
-  it('Mercadolibre is there', () => {
-      cy.contains('Ingresa');
-  });
-
-  it('filters_and_columns', () => {
-      cy.Login().wait(500);
-
-      cy.visit('https://www.mercadolibre.com.ar/publicaciones/editor-masivo/');
-      cy.wait(1000);
-
-      cy.get('.sc-bulk-items-quantity__text').then(($quantity) => {
-          const total = parseInt($quantity.text());
-          cy.contains('Filtros').parent().click();
-          cy.wait(1000);
-
-          cy.get('#ACTIVE').then(($activeCheck) => {
-              $activeCheck.click();
-              cy.contains('Aplicar').then(($applyBtn) => {
-                $applyBtn.click();
-                cy.wait(3000);
-                cy.get('.sc-bulk-items-quantity__text').then(($quantity) => {
-                    const itemsActive = parseInt($quantity.text());
-                    cy.log(itemsActive);
-                    cy.contains('Filtros').parent().click();
-                    cy.wait(3000);
-                    cy.get('#ACTIVE').click();
-                    cy.get('#INACTIVE').then(($inactiveCheck) => {                          
-                      $inactiveCheck.click();
-                      cy.wait(1000);
-                      cy.contains('Aplicar').then(($applyBtn) => {
-                        $applyBtn.click();
-                        cy.wait(5000);
-                        cy.get('.sc-bulk-items-quantity__text').then(($quantity) => {
-                            const itemsInactive = parseInt($quantity.text());
-                            cy.log(itemsInactive);
-                            cy.log(itemsActive);
-                            cy.log(total);
-                            expect(total).eq(itemsActive + itemsInactive);
-                        });
-                      });
-                    });
-                });
-              });
-          });
-      });  
+const { baseUrl } = require("../../../../config");
+beforeEach(() => {
+  cy.viewport(1920, 1080);
+  cy.visit("https://mercadolibre.com.ar");
+});
+describe("Level Three", () => {
+  it("active and inactive filters", () => {
+    cy.Login();
+    cy.waitForReact();
+    cy.visit(baseUrl);
+    cy.waitForReact();
+    cy.get(".sc-bulk-hover-button").contains("Filtros").click();
+    cy.get("#ACTIVE").click();
+    cy.get(".andes-button").contains("Aplicar").click();
+    cy.waitForReact();
+    cy.get(".sc-bulk-items-quantity__text").then(($el) => {
+      const text = $el.text();
+      const number = text.replace("publicaciones", "").trim();
+      cy.wrap(number).as("active");
+    });
+    cy.get(".sc-bulk-hover-button").contains("Filtros").click();
+    cy.get("#ACTIVE").click();
+    cy.get("#INACTIVE").click();
+    cy.get(".andes-button").contains("Aplicar").click();
+    cy.waitForReact();
+    cy.get(".sc-bulk-items-quantity__text").then(($el) => {
+      const text = $el.text();
+      const number = text.replace("publicaciones", "").trim();
+      cy.wrap(number).as("inactive");
+    });
   });
 });
